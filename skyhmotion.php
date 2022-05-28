d4f65e64d061c41431b0e153320bf6b56b970c30
rahul amin roktim <96077216+allintwo@users.noreply.github.com>
2022-05-28 13:35:57 +0600

    Wp_plugin_file

--- skyhmotion.php
+++ skyhmotion.php
@@ -5,3 +5,40 @@
  * Date: 5/28/2022
  * Time: 1:23 PM
  */
+
+/**
+ ** @package Amazon
+ ** @Heart Compatible
+ **/
+/*
+Plugin Name: Amazon review page builder
+Plugin URI: https://mimosait.com/?plugin=amazon
+Description: Amazon product review generator.
+Version: 1.0.0
+Author: CosMos
+Author URI: https://github.com/rahul-amin/
+License: GPLv2 or later
+Text Domain: skyhmotion
+*/
+/*
+Copyright (C) 2022 Faylab, https://faylab.com
+
+This program is not free software; so you can't redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ ** @author -fulldev CosMos
+ **/
+
+
+
+
